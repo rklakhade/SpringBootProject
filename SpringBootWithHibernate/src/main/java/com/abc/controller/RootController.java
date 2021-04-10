8462ee440063acc34d5138d8c21bed2d149f540d
Roshan <roshan.lakhade@gmail.com>
2021-04-10 18:10:08 +0530

    Added get method in Root Controller

--- SpringBootWithHibernate/src/main/java/com/abc/controller/RootController.java
+++ SpringBootWithHibernate/src/main/java/com/abc/controller/RootController.java
@@ -1,8 +1,19 @@
 package com.abc.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class RootController {
 
+	// get All Records
+	@GetMapping("/product/all")
+	public ResponseEntity<?> getAll() {
+		ResponseEntity<?> resp = null;
+			resp = new ResponseEntity<String>("Hi, This is test", HttpStatus.OK);
+		return resp;
+	}
 }
